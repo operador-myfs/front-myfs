7bcc24f8742e81e57ff56484fea38b8cd4641895
jporozco <jporozco@sistecredito.com>
2024-09-16 10:04:01 -0500

    loading

--- src/app/ver-documentos/ver-documentos.component.ts
+++ src/app/ver-documentos/ver-documentos.component.ts
@@ -1,33 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { VerDocumentosService } from './ver-documentos.service';
 import { CommonModule } from '@angular/common';
+import { LoadingService } from '../loading.interceptor';
 
 @Component({
   selector: 'app-ver-documentos',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './ver-documentos.component.html',
-  styleUrl: './ver-documentos.component.css'
+  styleUrl: './ver-documentos.component.css',
+  
 })
 export class VerDocumentosComponent {
 
   private docyumetnsService = inject(VerDocumentosService);
+  private loadingService = inject(LoadingService);
+
+  isLoading: boolean = false;
   documents: any = []
-  constructor() { }
+ 
 
   ngOnInit(): void {
     this.getDocuments();
   }
-
   getDocuments() {
     this.docyumetnsService.getDocuments().subscribe((data) => {
       this.documents = data.body;
     });
   }
   removeDocument(id: string) {
+    this.isLoading = true;
     this.docyumetnsService.deleteDocument(id).subscribe((response) => {
       if(!response.error)
-      this.getDocuments();
+        this.getDocuments();
+      this.isLoading = false;
     });
   }
 }
