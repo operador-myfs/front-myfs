ce3621d9e563827217f08d7254b093575221cb2d
jporozco <jporozco@sistecredito.com>
2024-10-05 09:26:35 -0500

    vercel final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    url: 'documents/',
-    urlUsuarios: 'cognito/',
+    url: 'https://ocrf9nzqde.execute-api.us-east-1.amazonaws.com/prod/documents/',
+    urlUsuarios: 'https://ocrf9nzqde.execute-api.us-east-1.amazonaws.com/prod/cognito/',
     documentsEndpoints: {
         documents: 'document',
         operators: 'operators',
